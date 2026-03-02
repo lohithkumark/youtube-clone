3bf3e2d5762bd426014e04b02cb7909475b8cb95
lohithkumar <lohitkumar2003@gmail.com>
2026-03-02 14:49:26 +0530

    Add protected route using auth middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./src/config/db.js";
 import authRoutes from "./src/routes/auth.routes.js";
+import { protect } from "./src/middleware/auth.middleware.js";
 
 dotenv.config();
 
@@ -14,8 +15,16 @@ app.use(express.json());
 // Auth routes
 app.use("/api/auth", authRoutes);
 
+
 app.get("/", (req, res) => {
   res.send("API is running...");
+});
+
+app.get("/api/protected", protect, (req, res) => {
+  res.json({
+    message: "You are authorized",
+    user: req.user,
+  });
 });
 
 const PORT = process.env.PORT || 8080;
