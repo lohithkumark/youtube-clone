b692f98b8b838b814165dfdd14922613ed0a6009
lohithkumar <lohitkumar2003@gmail.com>
2026-03-03 11:59:53 +0530

    Integrated Header and Sidebar into Home layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,20 @@
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <Header />
+
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+
+        <div style={{ padding: "20px", flex: 1 }}>
+          <h2>Video Grid Coming Soon...</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
